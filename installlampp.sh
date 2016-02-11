aca545ad46a586dc85227370e9d5ec5c3b1eb8e8
Tiago Floriano <tiagofloriano@gmail.com>
2016-02-11 07:59:16 -0200

    Inclusão do arquivo bash no repositório.

---
+++ installlampp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "(1) instalar LAMPP"; 
+echo "(2) remover LAMPP"; read RESP
+if [ $RESP -eq "1" ]
+then
+	sudo apt-get install apache2 mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt phpmyadmin
+fi
+if [ $RESP -eq "2" ]
+then
+	sudo apt-get remove apache2 mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt phpmyadmin
+fi
