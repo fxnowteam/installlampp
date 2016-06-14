c0ae39afb0be00c5d2ad8918274f6e2922f602d0
Tiago Floriano <tiagofloriano@gmail.com>
2016-06-14 09:02:37 -0300

    Retirada do pacote libapache2-mod-auth-mysql por estar ausente ou por estar bugado.

--- installlampp.sh
+++ installlampp.sh
@@ -3,9 +3,9 @@ echo "(1) instalar LAMPP";
 echo "(2) remover LAMPP"; read RESP
 if [ $RESP -eq "1" ]
 then
-	sudo apt-get install apache2 mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt phpmyadmin
+	sudo apt-get install apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt phpmyadmin
 fi
 if [ $RESP -eq "2" ]
 then
-	sudo apt-get remove apache2 mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt phpmyadmin
+	sudo apt-get remove apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt phpmyadmin
 fi
